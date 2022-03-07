5ec1936757d52c96e7d860127106bb87b970938b
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2022-03-07 15:06:14 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    return a + b;
+}
+// const a = 10;
+// const b = 20;
+var tong = sum(7, "5");
+console.log(tong);
---
+++ app.ts
@@ -0,0 +1,8 @@
+function sum(a:number,b:number){
+return a+b;
+}
+// const a = 10;
+// const b = 20;
+const tong = sum(7,"5");
+console.log(tong);
+
