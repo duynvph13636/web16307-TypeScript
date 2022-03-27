44ab43a03c491d831b7bd41efaf1e8fd46ca6b37
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2022-03-27 14:35:08 +0700

    signin

--- src/page/Singin.tsx
+++ src/page/Singin.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { UserType } from '../types/user';
+import {useForm} from "react-hook-form"
 
 type Props = {}
 type FormInput={
-  
+  email:String,
+  password:String
 }
 const Singin = (props: Props) => {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<FormInput>();
   return (
     <div>
       <form action="">
